13869a312ed55de42e5e7abfa1db3910c51c1032
Vaiibhavi_Singh <vaiibhavisingh855@gmail.com>
2026-01-22 14:06:37 +0530

    Add files via upload
    # Daily Notes
    
    ## Day 1
    - Learned how to print my personal info using C++.
    - Practiced basic input/output.
    
    ## Day 2
    - Wrote a program to check if a number is prime.
    - Improved my understanding of loops.

---
+++ c++6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age=18;
+    int roll_no= 2511026010769;
+    float pi=3.14;
+    char grade='A';
+    bool pass=true;
+    string name="Vaiibhavi Badmosh";
+
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Roll No:"<<roll_no<<endl;
+    cout<<"PI:"<<pi<<endl;
+    cout<<"Grade:"<<grade<<endl;
+    cout<<"Pass:"<<pass<<endl;
+
+    return 0;
+}
